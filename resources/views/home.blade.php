d521b73f9d9966f078e0dedbce875ba4441014bd
Michael_Hall <hallmichaelp@gmail.com>
2017-04-08 13:18:48 -0400

    Initial commit

---
+++ resources/views/home.blade.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <!-- Basic Page Needs
+    –––––––––––––––––––––––––––––––––––––––––––––––––– -->
+    <meta charset="utf-8">
+    <title>Michael Patrick Hall</title>
+    <meta name="description" content="Welcome to My Site">
+    <meta name="author" content="Michael Patrick Hall">
+
+    <!-- Mobile Specific Metas
+    –––––––––––––––––––––––––––––––––––––––––––––––––– -->
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <!-- FONT
+    –––––––––––––––––––––––––––––––––––––––––––––––––– -->
+    <link href="//fonts.googleapis.com/css?family=Raleway:400,300,600" rel="stylesheet" type="text/css">
+
+    <!-- CSS
+    –––––––––––––––––––––––––––––––––––––––––––––––––– -->
+    <link rel="stylesheet" href="css/frameworks/skeleton/normalize.css">
+    <link rel="stylesheet" href="css/frameworks/skeleton/skeleton.css">
+    <link rel="stylesheet" href="css/custom.css">
+
+  </head>
+
+  <body>
+
+  <div class="row">
+    <div class="twelve columns">
+      <h1 class="starter">Hi</h1>
+    </div>
+    </div>
+  </body>
+</html>
