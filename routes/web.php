becadd4e94866e70826d11545bcbb3ab9ce93129
Michael Hall <hallmichaelp@gmail.com>
2018-06-15 12:13:48 -0400

    updating two routes

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,5 @@
 <?php
 
 Route::get('/', 'HomeController@showHome');
-Route::get('/projects', 'ProjectController@ShowProject');
-Route::get('/headers', 'HeaderController@ShowHeader');
+Route::get('/projects', 'ProjectController@showProject');
+Route::get('/headers', 'HeaderController@showHeader');
